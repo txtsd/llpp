c4d7f364fb3e240279577fa3410ea6b3fee1e708
txtsd <code@ihavea.quest>
2023-11-18 12:03:02 +0530

    fix(ci): Build latest version of mupdf manually
    Signed-off-by: txtsd <code@ihavea.quest>

--- misc/getmupdf.sh
+++ misc/getmupdf.sh
@@ -3,22 +3,12 @@ set -eu
 
 MUPDF_OUTPUT_DIR="$1"
 u="git://git.ghostscript.com/mupdf"
-#u="https://github.com/ArtifexSoftware/mupdf"
 MUPDF_URL="${2-$u}"
-MUPDF_DESIRED_VERSION="f6ddaf30da1defe3be961f1172b83554bc6f6b48"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
-    git clone ${cloneargs-} ${MUPDF_URL} --recursive ${MUPDF_OUTPUT_DIR}
+    git clone ${cloneargs-} ${MUPDF_URL} ${MUPDF_OUTPUT_DIR}
+    cd ${MUPDF_OUTPUT_DIR}
+    git submodule update --init --recursive --jobs 0
 fi
 
-cd ${MUPDF_OUTPUT_DIR}
-git remote update
-MUPDF_VERSION=$(git rev-parse HEAD)
-
-test "${MUPDF_VERSION}" = "${MUPDF_DESIRED_VERSION}" || {
-    printf "mupdf current version is ${MUPDF_VERSION} "
-    echo "switching to ${MUPDF_DESIRED_VERSION}"
-    git reset --hard ${MUPDF_DESIRED_VERSION}
-    git submodule update --init --recursive
-}
